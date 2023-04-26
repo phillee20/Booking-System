1bdfc04e3dfa752b5fea29cb3f2be6c46ebc6188
phillee20 <phillee5647@hotmail.com>
2023-04-26 16:21:52 +0100

    UserContext added

--- Booking-System/api/index.js
+++ Booking-System/api/index.js
@@ -1,13 +1,15 @@
 const cors = require("cors");
 const express = require("express");
-const bcrypt = require("bcrypt");
-const mongoose = require("mongoose");
 const User = require("./model/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = "dfeeflnedfdmfejhvklfmdsf";
 
 //parses the json -  This solves the JSON error in console log
 app.use(express.json());
@@ -16,8 +18,9 @@ app.use(express.json());
 //Used yarn add CORS previously. Can set wild card * for allowing all origin but unsafe!
 app.use(
   cors({
+    //Set localhost to communicate to the endpoint
+    origin: "http://127.0.0.1:5173",
     credentials: true,
-    origin: "*", //Set what app can communicate to the API
   })
 );
 
@@ -31,14 +34,13 @@ app.get("/test", (request, response) => {
 
 app.post("/register", async (request, response) => {
   const { name, email, password } = request.body;
-
   try {
-    const userDoc = await User.create({
+    const userCred = await User.create({
       name,
       email,
       password: bcrypt.hashSync(password, bcryptSalt),
     });
-    response.json(userDoc);
+    response.json(userCred);
   } catch (error) {
     response.status(422).json(error);
   }
@@ -46,11 +48,25 @@ app.post("/register", async (request, response) => {
 
 app.post("/login", async (request, response) => {
   const { email, password } = request.body;
-  const userDoc = await User.findOne({ email });
-  if (userDoc) {
-    response.json("Found");
+  const userCred = await User.findOne({ email });
+  console.log(userCred);
+  if (userCred) {
+    const validPassword = bcrypt.compareSync(password, userCred.password);
+    if (validPassword) {
+      jwt.sign(
+        { email: userCred.email, id: userCred._id },
+        jwtSecret,
+        {},
+        (error, token) => {
+          if (error) throw error;
+          response.cookie("token", token).json(userCred);
+        }
+      );
+    } else {
+      response.status(422).json("Password is bad!");
+    }
   } else {
-    response.json("Not Found");
+    response.json("User not found");
   }
 });
 
