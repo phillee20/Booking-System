210389eae92215f78d861cd5fd76ce1c69c51477
phillee20 <phillee5647@hotmail.com>
2023-05-09 12:40:03 +0100

    home page

--- Booking-System/client/src/pages/IndexPage.jsx
+++ Booking-System/client/src/pages/IndexPage.jsx
@@ -1,3 +1,30 @@
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+
 export default function IndexPage() {
-  return <div>Index page here</div>;
+  const [places, setPlaces] = useState([]);
+
+  useEffect(() => {
+    axios.get("/places").then((response) => {
+      setPlaces(response.data);
+    });
+  }, []);
+
+  return (
+    <div className="mt-4">
+      {places.length > 0 && //List of saved places mapped from here
+        places.map((place) => (
+          <div className="flex w-32 h-32 bg-gray-300 grow shrink-0">
+            {place.photos?.[0] && (
+              <img
+                src={"http://localhost:4000/uploads/" + place.photos?.[0]}
+                alt=""
+              />
+            )}
+            {place.title}
+          </div>
+        ))}
+    </div>
+  );
 }
