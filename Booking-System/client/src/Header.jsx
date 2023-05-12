0e71ce5b632f63277fb1c9a3ec0c5cf88827b2e4
phillee20 <phillee5647@hotmail.com>
2023-05-12 12:56:58 +0100

    hide env files

--- Booking-System/client/src/Header.jsx
+++ Booking-System/client/src/Header.jsx
@@ -22,7 +22,7 @@ export default function Header() {
               d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
             />
           </svg>
-          <span className="font-bold text-xl">Travel Plus</span>
+          <span className="font-bold text-xl">AirBeeBee</span>
         </Link>
         <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
           <div>Anywhere</div>
